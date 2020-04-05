d0adf55f9d078fe5a9f399eb8806b6d961dd0b3e
Ignacio <cardozojoseignacio@gmail.com>
2020-04-05 12:22:49 -0300

    bd

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('pgsql');
+$CFG->dbtype    = getenv(pgsql);
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv( ec2-34-192-30-15.compute-1.amazonaws.com);
+$CFG->dbname    = getenv(dath48lkp109f9);
+$CFG->dbuser    = getenv(mjxziyroflcjvx);
+$CFG->dbpass    = getenv(050c91e2129b6e0201e76270831e55b2fb9d7370018c01d760f2cc870cc21a74);
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv(5432),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('http://moodle-isft190.herokuapp.com/');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('http://moodle-isft190.herokuapp.com');
+$CFG->dataroot  = getenv('/temp');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
