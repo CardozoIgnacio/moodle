617fee06420a3f575e08216aeae837c29464344e
Ignacio <cardozojoseignacio@gmail.com>
2020-04-06 11:07:54 -0300

    BdChange

--- config.php
+++ config.php
@@ -4,17 +4,17 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv(pgsql);
+$CFG->dbtype    = getenv('pgsql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv( ec2-34-192-30-15.compute-1.amazonaws.com);
-$CFG->dbname    = getenv(dath48lkp109f9);
-$CFG->dbuser    = getenv(mjxziyroflcjvx);
-$CFG->dbpass    = getenv(050c91e2129b6e0201e76270831e55b2fb9d7370018c01d760f2cc870cc21a74);
+$CFG->dbhost    = getenv( 'ec2-34-192-30-15.compute-1.amazonaws.com');
+$CFG->dbname    = getenv('dath48lkp109f9');
+$CFG->dbuser    = getenv('mjxziyroflcjvx');
+$CFG->dbpass    = getenv('050c91e2129b6e0201e76270831e55b2fb9d7370018c01d760f2cc870cc21a74');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => getenv(5432),
-  'dbsocket' => '',
+  'dbsocket' => ' ',
 );
 
 $CFG->wwwroot   = getenv('http://moodle-isft190.herokuapp.com');
